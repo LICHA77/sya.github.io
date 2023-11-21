6ea61f98c6d8b439cf32d39af91a7e0d89040732
UNMES <57911662+LICHA77@users.noreply.github.com>
2023-11-21 20:16:08 -0300

    Add files via upload

---
+++ esi.js
@@ -0,0 +1,40 @@
+function I1() {
+    document.getElementById("Q").style.display="flex";
+    document.getElementById("I1_").style.display="block";
+    document.getElementById("I1").style.display="none";
+}
+function I1_() {
+    document.getElementById("Q").style.display="none";
+    document.getElementById("I1_").style.display="none";
+    document.getElementById("I1").style.display="block";
+}
+function I2() {
+    document.getElementById("D").style.display="block";
+    document.getElementById("I2_").style.display="block";
+    document.getElementById("I2").style.display="none";
+}
+function I2_() {
+    document.getElementById("D").style.display="none";
+    document.getElementById("I2_").style.display="none";
+    document.getElementById("I2").style.display="block";
+}
+function I3() {
+    document.getElementById("P").style.display="block";
+    document.getElementById("I3_").style.display="block";
+    document.getElementById("I3").style.display="none";
+}
+function I3_() {
+    document.getElementById("P").style.display="none";
+    document.getElementById("I3_").style.display="none";
+    document.getElementById("I3").style.display="block";
+}
+function I4() {
+    document.getElementById("O").style.display="block";
+    document.getElementById("I4_").style.display="block";
+    document.getElementById("I4").style.display="none";
+}
+function I4_() {
+    document.getElementById("O").style.display="none";
+    document.getElementById("I4_").style.display="none";
+    document.getElementById("I4").style.display="block";
+}
---
+++ menu.js
@@ -0,0 +1,71 @@
+function mostrar() {
+    document.getElementById("sidebar").style.width = "13%";
+	document.getElementById("sidebar").style.height = "100%";
+    document.getElementById("abrir").style.display = "none";
+	document.getElementById("cerrar").style.display = "block";
+}
+function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("abrir").style.display = "inline";
+	document.getElementById("cerrar").style.display = "none";
+}
+function abrir1(){
+    document.getElementById("abrir1").style.display="none";
+    document.getElementById("cerrar1").style.display="block";
+    document.getElementById("ol1").style.display="block";
+    document.getElementById("ol1").style.height="auto";
+}
+function cerrar1(){
+    document.getElementById("abrir1").style.display="block";
+    document.getElementById("cerrar1").style.display="none";
+    document.getElementById("ol1").style.display="none";
+    document.getElementById("ol1").style.height="0";
+}
+function abrir2(){
+    document.getElementById("abrir2").style.display="none";
+    document.getElementById("cerrar2").style.display="block";
+    document.getElementById("ol2").style.display="block";
+    document.getElementById("ol2").style.height="auto";
+}
+function cerrar2(){
+    document.getElementById("abrir2").style.display="block";
+    document.getElementById("cerrar2").style.display="none";
+    document.getElementById("ol2").style.display="none";
+    document.getElementById("ol2").style.height="0";
+}
+function abrir3(){
+    document.getElementById("abrir3").style.display="none";
+    document.getElementById("cerrar3").style.display="block";
+    document.getElementById("ol3").style.display="block";
+    document.getElementById("ol3").style.height="auto";
+}
+function cerrar3(){
+    document.getElementById("abrir3").style.display="block";
+    document.getElementById("cerrar3").style.display="none";
+    document.getElementById("ol3").style.display="none";
+    document.getElementById("ol3").style.height="0";
+}
+function abrir4(){
+    document.getElementById("abrir4").style.display="none";
+    document.getElementById("cerrar4").style.display="block";
+    document.getElementById("ol4").style.display="block";
+    document.getElementById("ol4").style.height="auto";
+}
+function cerrar4(){
+    document.getElementById("abrir4").style.display="block";
+    document.getElementById("cerrar4").style.display="none";
+    document.getElementById("ol4").style.display="none";
+    document.getElementById("ol4").style.height="0";
+}
+function abrir5(){
+    document.getElementById("abrir5").style.display="none";
+    document.getElementById("cerrar5").style.display="block";
+    document.getElementById("ol5").style.display="block";
+    document.getElementById("ol5").style.height="auto";
+}
+function cerrar5(){
+    document.getElementById("abrir5").style.display="block";
+    document.getElementById("cerrar5").style.display="none";
+    document.getElementById("ol5").style.display="none";
+    document.getElementById("ol5").style.height="0";
+}
